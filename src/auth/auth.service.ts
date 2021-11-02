b9e77b1348e4c03de9c847a9a2a7d754042f5618
yhshin0 <asyh1047@naver.com>
2021-11-02 12:00:32 +0900

    feat: password hash 적용

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,23 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 import { UserCredentialsDto } from './dto/user-credentials.dto';
 import { User, UserDocument } from './user.schema';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService,
+  ) {}
 
   async createUser(userCredentialsDto: UserCredentialsDto): Promise<User> {
-    const createdUser = new this.userModel(userCredentialsDto);
+    const { username, password } = userCredentialsDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const createdUser = new this.userModel({ username, password: hashedPassword });
     return await createdUser.save();
   }
 
