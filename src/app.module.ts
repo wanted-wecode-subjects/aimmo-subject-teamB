19c6512060f7ffcca0ac38c0b2226d03c18dbf5c
yhshin0 <asyh1047@naver.com>
2021-11-02 14:59:24 +0900

    chore: comment scaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/aimmo'),
     AuthModule,
+    CommentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
