237b2bbe1368d7bd78b86d6c55b999c8b53a8e39
yhshin0 <asyh1047@naver.com>
2021-11-02 10:19:57 +0900

    chore: auth scaffolding

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+
+@Injectable()
+export class AuthService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+}
