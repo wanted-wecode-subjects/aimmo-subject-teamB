f917fac646d7210eee0ebf4575040e259f73dde4
yhshin0 <asyh1047@naver.com>
2021-11-02 10:48:21 +0900

    feat: 회원가입

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { UserCredentialsDto } from './dto/user-credentials.dto';
 import { User, UserDocument } from './user.schema';
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async createUser(userCredentialsDto: UserCredentialsDto): Promise<User> {
+    const createdUser = new this.userModel(userCredentialsDto);
+    return await createdUser.save();
+  }
 }
